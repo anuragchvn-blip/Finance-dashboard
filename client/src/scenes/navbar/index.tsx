445ab07fe71537a5f12991b710eab40417bccb82
anuragchvn1 <153076576+anuragchvn1@users.noreply.github.com>
2024-05-06 11:34:56 +0530

    change

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -4,19 +4,18 @@ import PixIcon from "@mui/icons-material/Pix";
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
-type NavbarProps = {};
+type Props = {};
 
-const Navbar = (props: NavbarProps) => {
+const Navbar = (props: Props) => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard");
-  
   return (
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* LEFT SIDE */}
       <FlexBetween gap="0.75rem">
         <PixIcon sx={{ fontSize: "28px" }} />
         <Typography variant="h4" fontSize="16px">
-          Finanseer
+          Finansweer
         </Typography>
       </FlexBetween>
 
@@ -31,7 +30,7 @@ const Navbar = (props: NavbarProps) => {
               textDecoration: "inherit",
             }}
           >
-            Dashboard
+            dashboard
           </Link>
         </Box>
         <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
@@ -43,7 +42,7 @@ const Navbar = (props: NavbarProps) => {
               textDecoration: "inherit",
             }}
           >
-            Predictions
+            predictions
           </Link>
         </Box>
       </FlexBetween>
